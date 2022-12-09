d6e5ba0df57c755be761fb1f64d605c48482bf39
Joe <phestr@gmail.com>
2022-12-08 17:25:40 -0700

    update

--- login.sh
+++ login.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #201 Ops Challenge Class 3
 #This script will print a greeting and then display the login history of the current machine
-greetings () {
+function greetings () {
 echo Hello $Name , here is the login history
-}
+};
 
 echo Enter First Name
 read Name
