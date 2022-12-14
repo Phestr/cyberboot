8db5df09a24b1df3d3df00bae7131e90909445f8
Joe <phestr@gmail.com>
2022-12-13 20:04:24 -0700

    update

---
+++ challenge5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#   stlouis-ops-201n3 Ops Challenge 5
+#   Author: Joe Doyle
+#Write a script that displays running processes, asks the user for a PID, then kills the process with that PID.
+#This loop will facilitate the menu system of your script, so that it can prompt the user to choose an option.
+ps aux
+
+P1=1
+
+while  [ $P1 -gt 0 ]
+do
+    echo Enter PID to terminate or 0 to quit:
+    read P1  
+    kill $P1
+    echo $P1 Terminated
+done
+
+echo Exit Successful
