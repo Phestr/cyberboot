e2249a5f22e71cf65422c57d151801685e378894
Joe <phestr@gmail.com>
2022-12-19 19:25:44 -0700

    update

--- challenge7.sh
+++ challenge7.sh
@@ -3,6 +3,7 @@
 # Ops Challenge Class 6 - System Information
 # Author:   Joe Doyle
 # Display system information
+
 echo CPU Info
 sudo lshw -class cpu | grep "product:\|vendor:\|physical id:\|bus info:\|width:"
 echo RAM Info
