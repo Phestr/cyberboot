33957c9783d38fade8ac6ea00a43a27f2d221f47
Joe <phestr@gmail.com>
2022-12-19 19:24:42 -0700

    update

---
+++ challenge7.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ops Challenge Class 6 - System Information
+# Author:   Joe Doyle
+# Display system information
+echo CPU Info
+sudo lshw -class cpu | grep "product:\|vendor:\|physical id:\|bus info:\|width:"
+echo RAM Info
+sudo lshw -class memory | grep "description:\|physical id:\|size:"
+echo Display Adapter Info
+sudo lshw -class network | grep "description\|product:\|vendor:\|physical id:\|bus info:\|width:\|clock:\|capabilities:\|configuration:\|resources:"
+echo Network Adapter Info:
+sudo lshw -class network | grep "description\|product:\|vendor:\|physical id:\|bus info:\|logical name:\|version:\|serial:\|size:\|capacity:\|width:\|clock:\|capabilities:\|configuration:\|resources:"
