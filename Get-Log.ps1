bd5ce2994ab1a63c41527e003b0511b55a7bf950
Joe <phestr@gmail.com>
2023-01-04 18:44:14 -0700

    update

--- Get-Log.ps1
+++ Get-Log.ps1
@@ -1,3 +1,7 @@
 
-Get-EventLog system -After (Get-Date).AddHours(-24) Out-File -FilePath home\joe\Desktop\24hrlog.txt
-Get-EventLog -LogName System -Entrytype error Out-File -FilePath home\joe\Desktop\Error.txt
+Get-EventLog system -After (Get-Date).AddHours(-24) | Out-File -FilePath home\joe\Desktop\last24.txt
+Get-EventLog -LogName System -Entrytype error | Out-File -FilePath home\joe\Desktop\Errors.txt
+Get-EventLog system -InstanceID 16
+Get-EventLog system -Newest 20
+$Events = Get-EventLog -LogName System -Newest 500
+>> $Events | Group-Object -Property Source -NoElement | Sort-Object -Property Count | Format-List
