809f444f0073282b8f9318394a52d0b9805363c5
Joe <phestr@gmail.com>
2023-01-09 19:27:02 -0700

    update

--- challenge13.sh
+++ challenge13.sh
@@ -2,14 +2,16 @@
 #Challenge 13
 #Domain Analyzer
 
+
+
 analyze () {
-    whois $instring >> output.txt
-    dig $instring >> output.txt
-    host $instring >> output.txt
-    nslookup $instring >> output.txt
+    whois $instring 
+    dig $instring  
+    host $instring  
+    nslookup $instring 
 }
 
 echo Input domain name:
 read instring
-analyze $instring
+analyze > output.txt
 echo Analysis written to output.text
