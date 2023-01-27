37adb5ea1153d2830c9382f7ff82301fac97aa43
Joe <phestr@gmail.com>
2023-01-26 17:46:08 -0700

    update

--- opsch04.sh
+++ opsch04.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 #Ops301 Challenge 4
+#Create a bash script that launches a menu system with the following options:
+#1. Hello world (prints “Hello world!” to the screen)
+#2. Ping self (pings this computer’s loopback address)
+#3. IP info (print the network adapter information for this computer)
+#4. Exit
+#Next, the user input should be requested.
+#The program should next use a conditional statement to evaluate the user’s input, then act according to what the user selected.
+#Use a loop to bring up the menu again after the request has been executed.
 #Author: Joe Doyle
 
 e=y
@@ -23,6 +31,6 @@ do
        exit
     fi
 
-    echo "Do you want to go again?"
+    echo "Do you want to go again? (y/n)"
     read e
 done
