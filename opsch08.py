ac7de2cc56e09760dc4baf85e6f674aa62257f41
Phestr <phestr@gmail.com>
2023-02-14 09:15:14 -0700

    update

--- opsch08.py
+++ opsch08.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 # Ops 301 Challenge 8 : Python Collections
 
-
-
-
 # Assign to a variable a list of ten string elements.
 my_produce_list = [ "carrot", "celery", "spinach", "squash", "potato", "turnip", "avocado", "apple", "orange", "lime" ]
 
