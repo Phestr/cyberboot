f6cceec13e1b98d107ab2dbb2773f034f715443d
Joe <phestr@gmail.com>
2023-03-08 17:31:19 -0700

    update

---
+++ opsch11.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#Ops301 Challenge Class 11
+#Author: Joe Doyle
+import psutil
+#Create a Python script that fetches this information using Psutil:
+
+#Time spent by normal processes executing in user mode
+
+print(psutil.cpu_times())
+
+
